82a06c8420796badc8cd6d30c4c179c8902d2be1
Robert Henryk Zawiasa <robert.zawiasa@gmail.com>
2017-05-05 17:53:19 +0200

    70505e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # shrdlu
 SHRDLU [to be] resurrected in Clojure
+[![Build Status](https://travis-ci.org/bodza/shrdlu.svg?branch=master)](https://travis-ci.org/bodza/shrdlu)
--- src/shrdlu/core.clj
+++ src/shrdlu/core.clj
@@ -174,7 +174,7 @@
     ([f s] (when-let [s (seq s)] (lazy-seq (cons (f s) (map* f (cdr s)))))))
 (defn- subst [x y z] (replace {y x} z))
 
-(declare evlis thfail thtrue thadd- thadd thadd* thamong thamong'f thand thand'f thand't thor thor- thor'f thor't thcond thcond'f thcond't thapply thapply1 thtae thass- thassert therase thassert'f thassert't therase'f therase't thgal thasval thbind thbranch thbranchun thdo thdo'b thfinalize thfind thfind'f thfind't thgo thtry thgoal thgoal'f thgoal't thurplaca thurplacd thrplacas thrplacds thrplaca thrplacd thmatch2 thmatch1 thmatch thmung'f thmung't thnot thprog thprog- thprog'f thprog't thpure thputprop thremprop thrembind'f thrembind't assq- threm- thremove threturn thsucceed thtag thtag'f thtag't thtry- thundo'f thundo't thv- thv thnv thval thvar? thvars2 thvarsubst thsgal thsetq thvsetq thante thconse therasing thdef topcenter atab clear diff half endtime findspace grow locgreater memorend memory occupier order packo packon packord size startime support tcent tfind timechk combination? findb from meet setmod setdif sta union uppercase-ify-char ETAOIN propname wordprops! undefined! setmvb add-f remove-f one-word-left move-pt move-ptw apply-grammar buildnode rebuild word features firstword wordafter daughters semantics parent root cut cut-back-one flushme following previous m! nextword nextword? parse parse2 parse3 parserel pop* popto cq f! feset fq! isq nq rq trnsf passing spread1 grammar CLAUSE NG VG PREPG ADJG CONJOIN conjo comma cantake canparse both thank !blueprint !build !color !cleanoff !eqdim !grasp !have !in !loc !name !notice !on !propdefine !stackup smtime smngtime smincom smthat smconj- smconj smvg smpron smvaux smplace smtoadj smadverb smprop smadjqshort smadjg-prepg smit- smit smngof smng1 smng2 smng3 smone smone2 smone3 smposs smposs2 smrelate smcl1 smcl2 smcl-modifiers smbind smbinder istense imperf? build newcopy relation dobackref mapbland iter- iterate* object junkify- plnr-junkify plnr-thconsify plnr-findify plnr-findspec mumble plnr-goalify plnr-mung plnr-notify plnr-progify plnr-numrel plnr-numsub plnr-recommendify plnr-remtime compare-build findmeasure measure plnr-describe relfind ordmake compare-proc erqset expand thval2 who check-a-marker check-markers findevents checkrel action? ambiguities? conjuncts? ansrss? determiner? end? markers? negative? num? disjuncts? oss? parsenode? plausibility? plnrcode? qtype? quantifier? refer? rel? relations? relmarkers? rss? rssvar? start? systems? tense? tss? variable? smset answer ansbuild anscommand ansdeclare anseliminate parse-assoc ansgen ansname ansnorel ansorder ansquest ansrel ansthm+ ansthm1 ansthm ansunique cutoff describevent disput eliza enough-better findmother headpart onecheck listnames pron-prt nameaction namelist namelist-evaled namenum ansay nameobj namesize namesugar notell ordname plnr-andorify prepput pluralize pluralmake thval-mult toplevel findreduce findchoose vbfix showscene shrdlu)
+(declare evlis thfail thtrue thadd- thadd thadd* thamong thamong'f thand thand'f thand't thor thor- thor'f thor't thcond thcond'f thcond't thapply- thapply thtae thass- thassert therase thassert'f thassert't therase'f therase't thgal thasval thbind thbranch thbranchun thdo thdo'b thfinalize thfind thfind'f thfind't thgo thtry thgoal thgoal'f thgoal't thurplaca thurplacd thrplacas thrplacds thrplaca thrplacd thmatch2 thmatch1 thmatch thmung'f thmung't thnot thprog thprog- thprog'f thprog't thpure thputprop thremprop thrembind'f thrembind't assq- threm- thremove threturn thsucceed thtag thtag'f thtag't thtry- thundo'f thundo't thv- thv thnv thval thvar? thvars2 thvarsubst thsgal thsetq thvsetq thante thconse therasing thdef topcenter atab clear diff half endtime findspace grow locgreater memorend memory occupier order packo packon packord size startime support tcent tfind timechk combination? findb from meet setmod setdif sta union uppercase-ify-char ETAOIN propname wordprops! undefined! setmvb add-f remove-f one-word-left move-pt move-ptw apply-grammar buildnode rebuild word features firstword wordafter daughters semantics parent root cut cut-back-one flushme following previous m! nextword nextword? parse parse2 parse3 parserel pop* popto cq f! feset fq! isq nq rq trnsf passing spread1 grammar CLAUSE NG VG PREPG ADJG CONJOIN conjo comma cantake canparse both thank !blueprint !build !color !cleanoff !eqdim !grasp !have !in !loc !name !notice !on !propdefine !stackup smtime smngtime smincom smthat smconj- smconj smvg smpron smvaux smplace smtoadj smadverb smprop smadjqshort smadjg-prepg smit- smit smngof smng1 smng2 smng3 smone smone2 smone3 smposs smposs2 smrelate smcl1 smcl2 smcl-modifiers smbind smbinder istense imperf? build newcopy relation dobackref mapbland iter- iterate* object junkify- plnr-junkify plnr-thconsify plnr-findify plnr-findspec mumble plnr-goalify plnr-mung plnr-notify plnr-progify plnr-numrel plnr-numsub plnr-recommendify plnr-remtime compare-build findmeasure measure plnr-describe relfind ordmake compare-proc erqset expand thval2 who check-a-marker check-markers findevents checkrel action? ambiguities? conjuncts? ansrss? determiner? end? markers? negative? num? disjuncts? oss? parsenode? plausibility? plnrcode? qtype? quantifier? refer? rel? relations? relmarkers? rss? rssvar? start? systems? tense? tss? variable? smset answer ansbuild anscommand ansdeclare anseliminate parse-assoc ansgen ansname ansnorel ansorder ansquest ansrel ansthm+ ansthm1 ansthm ansunique cutoff describevent disput eliza enough-better findmother headpart onecheck listnames pron-prt nameaction namelist namelist-evaled namenum ansay nameobj namesize namesugar notell ordname plnr-andorify prepput pluralize pluralmake thval-mult toplevel findreduce findchoose vbfix showscene shrdlu)
 
 (putprop! 'EE :why :command)
 (putprop! 'EE :start 0)
@@ -365,11 +365,7 @@
 
 (defn- thpure [x] (not-any? thvar? x))
 
-(defn- thapply [x y]
-    ;; THAPPLY1 DOES THE REAL WORK, ALL WE DO IS GET THE THEOREM OFF THE PROPERTY LIST.
-    (thapply1 (getprop x :theorem) y))
-
-(defn- thapply1 [b d]
+(defn- thapply- [b d]
     (thbind (cadr b))
     ;; MAKE SURE THE THEOREM PATTERN MATCHES THE GOAL.
     (if (thmatch1 d (caddr b))
@@ -378,6 +374,10 @@
         (do (thprog- (cddr b) nil) true)
         ;; IF THE THEOREM PATTERN DIDN'T MATCH, START FAILING.
         (do (set! *vars* *vars0*) (thpop! *tree*) nil)))
+
+(defn- thapply [x y]
+    ;; THAPPLY1 DOES THE REAL WORK, ALL WE DO IS GET THE THEOREM OFF THE PROPERTY LIST.
+    (thapply- (getprop x :theorem) y))
 
 (defn- thtae [a x1 t m]
     (cond (term? a) nil
@@ -613,7 +613,7 @@
             :thtbf (loop [b b]
                     (let-when [[_ b2 b3] (car b)] b3 => (recur (cdr b))
                         (let-when [t (car b3) w (getprop t :theorem)] (and w (= (car w) :thconse)) => (bug! 'thtry- "BAD THEOREM" t)
-                            (let-when [? (and (b2 (car b3)) (thapply1 w a)) b (cons [:thtbf b2 (cdr b3)] (cdr b))] ? => (recur b)
+                            (let-when [? (and (b2 (car b3)) (thapply- w a)) b (cons [:thtbf b2 (cdr b3)] (cdr b))] ? => (recur b)
                                 (thpush! *tree* [:thgoal a b n])
                                 true)))))))
 
